0d35b8c2fecb293b7f020790f66d5f2bdc89b8ec
Magnus <Magnus.bryntheson@telia.com>
2018-06-10 11:30:05 +0200

    First Upload

---
+++ karta.js
@@ -0,0 +1,59 @@
+function initMap() {
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 58.94956329999999, lng: 12.214956099999995},
+    zoom: 17
+  });
+
+  var marker = new google.maps.Marker({
+  	position: {lat: 58.94956329999999, lng: 12.214956099999995},
+  	map: map
+  })
+
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+  		console.log(pos);
+      map.setCenter(pos);
+  		calcRoute(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    }, {timeout:10000});
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'Error: The Geolocation service failed.' :
+                          'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
+
+  var directionsDisplay = new google.maps.DirectionsRenderer();
+  var directionsService = new google.maps.DirectionsService();
+
+  directionsDisplay.setMap(map);
+  function calcRoute(pos) {
+    var start = pos;
+    var end = {lat: 58.94956329999999, lng: 12.214956099999995};
+    var request = {
+      origin: start,
+      destination: end,
+      travelMode: 'DRIVING'
+    };
+
+    directionsService.route(request, function(result, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(result);
+      }
+    });
+  }
+}
